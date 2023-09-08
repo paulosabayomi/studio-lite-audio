46261d4bbd7edfe8b12c5611d47d730a27e34267
Abayomi Amusa <amusaabayomipaul99@gmail.com>
2023-09-08 01:55:53 +0100

    I changed some things in Sonobus and personalized it for Flow Basket Inc

---
+++ Source/SonobusTypes.h
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPLv3-or-later WITH Appstore-exception
+// Copyright (C) 2020 Jesse Chappell
+
+#pragma once
+
+
+class SonobusCommands
+{
+public:
+    
+    enum {
+        MuteAllInput = 1,
+        MuteAllPeers,
+        TogglePlayPause,
+        ToggleLoop,
+        TrimSelectionToNewFile,
+        CloseFile,
+        Connect,
+        Disconnect,
+        ShareFile,
+        RevealFile,
+        ShowOptions,
+        OpenFile,
+        RecordToggle,
+        CheckForNewVersion,
+        LoadSetupFile,
+        SaveSetupFile,
+        ChatToggle,
+        SoundboardToggle,
+        SkipBack,
+        ShowFileMenu,
+        ShowTransportMenu,
+        ShowViewMenu,
+        ShowConnectMenu,
+        ToggleFullInfoView,
+        StopAllSoundboardPlayback,
+        ToggleAllMonitorDelay
+    };
+    
+};
